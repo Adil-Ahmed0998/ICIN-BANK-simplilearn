2b70f839c71106e3fcb80d2993d3ec43ddc30321
Adil-Ahmed0998 <129112836+Adil-Ahmed0998@users.noreply.github.com>
2023-11-19 03:18:28 +0530

    Second

---
+++ src/main/java/com/Adil Ahmed/onlinebanking/repository/PrimaryAccountDao.java
@@ -0,0 +1,9 @@
+package com.hendisantika.onlinebanking.repository;
+
+import com.hendisantika.onlinebanking.entity.PrimaryAccount;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrimaryAccountDao extends CrudRepository<PrimaryAccount, Long> {
+
+    PrimaryAccount findByAccountNumber(int accountNumber);
+}
---
+++ src/main/java/com/Adil Ahmed/onlinebanking/repository/UserDao.java
@@ -0,0 +1,17 @@
+package com.hendisantika.onlinebanking.repository;
+
+import com.hendisantika.onlinebanking.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+
+public interface UserDao extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    List<User> findAll();
+}
